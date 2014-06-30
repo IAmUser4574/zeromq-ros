e562a3a5b7d6648dc897d450bf23ee71a1847e51
Alexander Wallar <aw204@st-andrews.ac.uk>
2014-06-30 15:26:35 -0400

    Added scripts for ease

---
+++ scripts/run_ns.sh
@@ -0,0 +1,2 @@
+
+python run.py --host 132.250.85.150 --port 8080 --program nameserver --config configs/pelicans.json
---
+++ scripts/run_uav.sh
@@ -0,0 +1,2 @@
+
+python run.py --host 132.250.85.150 --port 8080 --program client --name $1

b59b4e9f4989b79e1bb0fe947c2168a5c4c5df87
Alexander Wallar <aw204@st-andrews.ac.uk>
2014-07-10 11:48:29 -0400

    Change run_ns.sh

--- scripts/run_ns.sh
+++ scripts/run_ns.sh
@@ -1,2 +1,2 @@
 
-python run.py --host $ZMQROS_NS_HOST --port $ZMQROS_NS_PORT --program nameserver --config configs/pelicans.json
+python run.py --host $ZMQROS_NS_HOST --port $ZMQROS_NS_PORT --program nameserver --config $1
